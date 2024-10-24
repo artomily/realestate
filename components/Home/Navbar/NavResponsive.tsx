eb335a7feaa468187b9fe05e023abcf2a9f7c87f
rakyavaraartomily <rakyavara.artomily@sigma.co.id>
2024-10-25 01:00:19 +0700

    The Beginning

---
+++ components/Home/Navbar/NavResponsive.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav'
+import NavMobile from './NavMobile'
+
+const NavResponsive = () => {
+  return (
+    <div>
+          <Nav />
+          <NavMobile/>
+    </div>
+  )
+}
+
+export default NavResponsive
